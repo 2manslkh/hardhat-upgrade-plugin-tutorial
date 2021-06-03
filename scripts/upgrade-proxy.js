9e84bb7f7920a6f4cf127b089da4908676efae33
Lim Keng Hin <kenghin_lim@hotmail.com>
2021-06-03 12:06:36 +0800

    Update comments

--- scripts/upgrade-proxy.js
+++ scripts/upgrade-proxy.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   // Upgrades Proxy to use LogicTwo
-  const PROXY_ADDRESS = "0xBC93b0ceA1Da41659c6fa0B51dBBC78700041c3c"; // Change PROXY_ADDRESS to address where your proxy is deployed
+  const PROXY_ADDRESS = "0xFFc6f702512c41F8dDF5Ca393135606026a34d7F"; // Change PROXY_ADDRESS to address where your proxy is deployed
   const LogicTwo = await ethers.getContractFactory("LogicTwo");
   const Proxy = await upgrades.upgradeProxy(PROXY_ADDRESS, LogicTwo);
   console.log("Proxy upgraded");
