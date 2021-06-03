7f22c431670dba4fcb6e4dc77ecb32a91ca81530
Lim Keng Hin <kenghin_lim@hotmail.com>
2021-06-03 12:07:02 +0800

    Update comments

--- scripts/call-proxy-web3.js
+++ scripts/call-proxy-web3.js
@@ -3,7 +3,7 @@ const { web3 } = require("hardhat");
 async function main() {
   // web3 instance is already created by hardhat
 
-  const PROXY_ADDRESS = "0xBC93b0ceA1Da41659c6fa0B51dBBC78700041c3c";
+  const PROXY_ADDRESS = "0xBC93b0ceA1Da41659c6fa0B51dBBC78700041c3c"; // Change PROXY_ADDRESS to address where your proxy is deployed
   const implementation = await hre.artifacts.readArtifact("LogicTwo");
   const implementationABI = implementation.abi;
   const proxy = new web3.eth.Contract(implementationABI, PROXY_ADDRESS);
